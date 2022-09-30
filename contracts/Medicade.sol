cf7df3bca7e24497b6e4c54f27636c946e794d35
cosisaxis <kishanelewis2@gmail.com>
2022-09-29 19:37:41 -0500

    medicade-event

--- contracts/Medicade.sol
+++ contracts/Medicade.sol
@@ -5,11 +5,14 @@ contract Medicade {
     uint256 age;
     string condition;
 
+    event medRecordsUp(string _name, uint256 _age, string _condition);
+
     function medRecords(string memory _name, uint256 _age, string memory _condition) public {
         name = _name;
         age = _age;
         condition = _condition;
         require(age >= 18, "you are not old enough");
+        emit medRecordsUp(_name, _age, _condition);
     }
     
     function getMedRecords() public view returns(string memory, uint256, string memory){
